a07496f9cbda5c76f5142bd6cfbc28eb26d626c9
adylewsk <adylewsk@student.42.fr>
2021-06-03 19:21:26 +0200

    Norminette 3.0

--- includes/struct.h
+++ includes/struct.h
@@ -6,7 +6,7 @@
 /*   By: adylewsk <adylewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/03 17:14:55 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/06/03 19:02:56 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/06/03 19:05:28 by adylewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,6 @@ typedef struct s_list
 	s_list	*prev;
 	int		nbr;
 	s_list	*next;
-
 }	t_list;
 
 #endif
