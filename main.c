0ce28010755f8af8adb37c38c2fee4f335525424
adylewsk <adylewsk@student.42.fr>
2021-06-08 17:26:27 +0200

    add splits

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: adylewsk <adylewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/01 16:35:43 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/06/08 15:39:17 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/06/08 17:15:43 by adylewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,5 @@ int	main(int argc, char **argv)
 	if (check_args(argc, argv) == FALSE)
 		exit(my_error("./push_swap [NUMBER] ...", 1, EXIT_FAILURE));
 	ft_printf("check args ok\n");
-	ft_split("fsdfa sa",' ');
-	ft_printf("%i",ft_atoi("17"));
 	return (0);
 }
