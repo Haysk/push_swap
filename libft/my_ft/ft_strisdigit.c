2342327e6c8b52e02a5a6be68df1ca2add1e2ba9
adylewsk <adylewsk@student.42.fr>
2021-06-10 00:55:15 +0200

    add neg nbr

--- libft/my_ft/ft_strisdigit.c
+++ libft/my_ft/ft_strisdigit.c
@@ -6,7 +6,7 @@
 /*   By: adylewsk <adylewsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/10 17:20:04 by adylewsk          #+#    #+#             */
-/*   Updated: 2021/06/03 00:14:56 by adylewsk         ###   ########.fr       */
+/*   Updated: 2021/06/09 22:32:49 by adylewsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,20 @@
 
 int	ft_strisdigit(char *str)
 {
+	int	sign;
+
+	sign = 0;
 	if (str == NULL)
 		return (FALSE);
 	while (*str)
 	{
-		if (!ft_isdigit(*str))
+		if (*str == '-')
+		{
+			if (sign != 0)
+				return (FALSE);
+			sign++;
+		}
+		else if (!ft_isdigit(*str))
 			return (FALSE);
 		str++;
 	}
